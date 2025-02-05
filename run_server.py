71e3f6fa07be7493c9ca635a8698a52072fff8a5
Will <will@WilldeMacBook-Pro.local>
2025-02-05 16:37:47 +0800

    a

--- run_server.py
+++ run_server.py
@@ -10,13 +10,13 @@ def main():
         #rss_server.add_source("https://www.reddit.com/r/programming/.rss", "Reddit Programming")
         rss_server.add_source("https://rss.panewslab.com/en/gtimg/rss", "PANews")
     
-        while True:
+        while True: 
             print("Checking for updates...")
             rss_server.check_updates()
             time.sleep(5*60)  # 5 minutes
     except Exception as e:
         print(f"An error occurred: {e}")
-        time.sleep(10)  # Wait for 10 seconds before restarting
+        time.sleep(300)  # Wait for 10 seconds before restarting
 
 if __name__ == "__main__":
     main() 
